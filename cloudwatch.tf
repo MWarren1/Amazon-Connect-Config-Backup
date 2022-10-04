58b790a782f7fb32d4b4e69d861061617aaa36ba
hyde-mwarren <matthew.warren@hyde-housing.co.uk>
2022-10-04 08:51:14 +0100

    correcitng log group name

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,6 +1,6 @@
 # log group for  lambda fuction
 resource "aws_cloudwatch_log_group" "log_group" {
-  name              = local.lambda_function_name
+  name              = "/aws/lambda/${local.lambda_function_name}"
   retention_in_days = var.cloudwatch_log_group_retention
 
   tags = var.parent_tags
