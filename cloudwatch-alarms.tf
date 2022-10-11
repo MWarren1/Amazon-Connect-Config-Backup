3093f6979483d3a57aa9f621df6c268292dddd33
hyde-mwarren <matthew.warren@hyde-housing.co.uk>
2022-10-11 22:12:09 +0100

    updating alarm

--- cloudwatch-alarms.tf
+++ cloudwatch-alarms.tf
@@ -5,21 +5,28 @@ resource "aws_cloudwatch_metric_alarm" "failed_backup" {
   alarm_description  = "Trigger an alert when amazon connect backup has failed"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
-  threshold           = "0"
-  metric_query {
-    id = "m1"
-    metric {
-      metric_name = "Errors"
-      namespace   = "AWS/Lambda"
-      period      = "120"
-      stat        = "Sum"
-      unit        = "Count"
 
-      dimensions = {
+  metric_name = "Errors"
+  namespace   = "AWS/Lambda"
+  period      = "120"
+  threshold   = "0"
+  statistic   = "Sum"
+  dimensions = {
         FunctionName = local.lambda_function_name
       }
-    }
-  }
+  # metric_query {
+  #   id = "m1"
+  #   metric {
+  #     metric_name = "Errors"
+  #     namespace   = "AWS/Lambda"
+  #     period      = "1"
+  #     stat        = "Sum"
+  #     unit        = "Count"
+  #     dimensions = {
+  #       FunctionName = local.lambda_function_name
+  #     }
+  #   }
+  # }
  
   alarm_actions      = [aws_sns_topic.topic[0].arn]
   treat_missing_data = "missing"
