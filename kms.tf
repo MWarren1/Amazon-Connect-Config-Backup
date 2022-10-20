987fd718d4b0274479872465176c4d477dd3882c
hyde-mwarren <matthew.warren@hyde-housing.co.uk>
2022-10-20 21:35:33 +0100

    updatinf kms policy

--- kms.tf
+++ kms.tf
@@ -2,18 +2,33 @@ resource "aws_kms_key" "sns" {
   count = var.encyrpt_sns ? 1 : 0
 
   description = "amazon connect backup - used for sns encryption"
-  policy = <<POLICY
+  policy      = <<POLICY
   {
-    "Sid": "Allow_Publish_Alarms",
-    "Effect": "Allow",
-    "Principal":
-    {
-        "Service": [
-            "cloudwatch.amazonaws.com"
-        ]
-    },
-    "Action": "sns:Publish",
-    "Resource": "*"
+    "Version": "2012-10-17",
+    "Id": "key-default-1",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::119511693873:root"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow_CloudWatch_alarms",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudwatch.amazonaws.com"
+            },
+            "Action": [
+                "kms:Decrypt",
+                "kms:GenerateDataKey*"
+            ],
+            "Resource": "*"
+        }
+    ]
 }
 POLICY
 
