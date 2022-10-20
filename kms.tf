57fec8cb5bc3db1e982f701460600f76679501c6
hyde-mwarren <matthew.warren@hyde-housing.co.uk>
2022-10-20 20:22:53 +0100

    spelling mistake

--- kms.tf
+++ kms.tf
@@ -7,7 +7,7 @@ resource "aws_kms_key" "sns" {
 
 resource "aws_kms_alias" "sns" {
   count = var.encyrpt_sns ? 1 : 0
-  
-  name          = "alias/amamzon-connect-backup-sns-${random_id.rand.dec}"
+
+  name          = "alias/amazon-connect-backup-sns-${random_id.rand.dec}"
   target_key_id = aws_kms_key.sns[0].key_id
 }
