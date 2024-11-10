fd868f338602a1dde33897d7227291d7396b23a9
hyde-mwarren <matthew.warren@hyde-housing.co.uk>
2024-11-10 19:26:32 +0000

    fix(python): replacing old build.sh with terraform

--- lambda.tf
+++ lambda.tf
@@ -8,20 +8,30 @@ locals {
 }
 
 # Lambda function backup amazon connect
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/app-code/src/connect_backup"
+  output_path = "${path.module}/app-code/dist/connect_backup.zip"
+}
+
 resource "aws_lambda_function" "connect_backup" {
   description      = "this lambda backups up amazon connect instances"
-  filename         = "${path.module}/app-code/dist/connect_backup.zip"
+  filename         = data.archive_file.lambda_zip.output_path
   function_name    = local.lambda_function_name
   role             = aws_iam_role.lambda_role.arn
   handler          = "connect_backup.lambda_handler"
-  source_code_hash = filebase64sha256("${path.module}/app-code/dist/connect_backup.zip")
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.9"
   memory_size      = 128
   timeout          = 900
   environment {
     variables = {
-      OUTPUT_S3_BUCKET = aws_s3_bucket.connect_backup.id
+      DAILY_BACKUP_S3_BUCKET  = aws_s3_bucket.connect_backup_daily.id
+      WEEKLY_BACKUP_S3_BUCKET = aws_s3_bucket.connect_backup_weekly.id
     }
+  }
+  tracing_config {
+    mode = "Active"
   }
   tags = var.parent_tags
 }
